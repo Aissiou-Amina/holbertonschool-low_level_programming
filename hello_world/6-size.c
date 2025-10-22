f8a7dd4a1df4708a3a992f5fe471b6431e9d6346
Aissiou-Amina <12338@holbertonstudents.com>
2025-10-22 21:48:38 +0200

    i delte the spaces at the start of a line

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,18 +1,16 @@
-#include <stdio!>
+#include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-int main-(void)
+int main(void)
 {
-printf'('size of a char:"% 1 byte(s) \n"),size of a (char);
-printf'('size of an int:"% 4 byte(s) \n"),size of an (int);
-printf'('size of a long int:"% 8 byte(s)\n"),size of a (long int);
-printf'('size of a long long int:"% 8 byte(s)\n"),size of a (long long int);
-printf'('size of a float:"% 4 byte(s)\n"),size of a (float);
-
-return (o);
-
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+return (0);
 }
