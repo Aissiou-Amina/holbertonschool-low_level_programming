24b3dfa3e720050ebda79ae5be512303fbc621d5
Aissiou-Amina <12338@holbertonstudents.com>
2025-10-31 16:31:08 +0100

    i add a new prototype named void rev_string(char *s);

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,10 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
+void print_rev(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
+void rev_string(char *s);
 #endif
