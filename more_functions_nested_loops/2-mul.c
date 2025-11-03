f5218c425f18d9d1ee62435620a8f8e79517e976
Aissiou-Amina <12338@holbertonstudents.com>
2025-11-03 13:11:54 +0100

    add the description

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - that myltiplies
+ * @a: - that multiplies two integers
+ * @b: - that multiplies two integers
+ * Return: (0)
+ *
+ */
+int mul(int a, int b)
+{
+	int n = a * b;
+
+	return (n);
+}
