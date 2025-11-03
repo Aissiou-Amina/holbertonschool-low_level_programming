41526520d0f013130b33aec802deabfeff459819
Aissiou-Amina <12338@holbertonstudents.com>
2025-11-03 14:10:15 +0100

    Add the comment for betty

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * print_numbers - function that print the numbers,
+ * @void - 0 to 9, followed by a new line
+ * Return: Always 0 (Success)
+ */
 void print_numbers(void)
 {
 	int n = 48;
