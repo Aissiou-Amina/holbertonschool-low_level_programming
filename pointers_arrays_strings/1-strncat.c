b974d33bdc8926fdd14b17c6fc0f69a9744c174c
Aissiou-Amina <12338@holbertonstudents.com>
2025-11-04 10:04:09 +0100

    Write a function that concatenates two strings.

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n: maximum number of characters to add from src
+ *
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+
+	for (j = 0; j < n && src[j] != '\0'; j++, i++)
+		dest[i] = src[j];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
