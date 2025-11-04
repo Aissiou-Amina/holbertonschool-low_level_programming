e25cfdfae70829cffbcf43e839a7acee0b58a39f
Aissiou-Amina <12338@holbertonstudents.com>
2025-11-04 13:59:50 +0100

    Write a function that copies a string.

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncyp - copies a string up to n characters
+ * @dest _distination string
+ * @src _source string
+ * @n: maximum number of characters to copy
+ *
+ * Return: pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (; i < n ; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
