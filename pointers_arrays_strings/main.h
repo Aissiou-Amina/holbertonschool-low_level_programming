8b653a14dfa882309a450023d0208126202279a8
Aissiou-Amina <12338@holbertonstudents.com>
2025-11-05 15:26:30 +0100

    i add a new prototype unsigned int _strspn(char *s, char *accept);

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -19,4 +19,7 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *_memset(char *s, char b, unsigned int n);
+char *_strchr(char *s, char c);
+char *_memcpy(char *dest, char *src, unsigned int n);
+unsigned int _strspn(char *s, char *accept);
 #endif
